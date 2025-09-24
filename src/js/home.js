c376e4a2d1e9281029f51b625316f4aa1103567d
tia-zaid <rhieskhiez.creative@gmail.com>
2025-09-25 00:11:49 +0700

    Update home.js

--- src/js/home.js
+++ src/js/home.js
@@ -10,7 +10,7 @@ export const home = () => {
         return `
             <img src="${coupleImage}" alt="couple animation">
             <figcaption>
-                ${brideLName.split(' ')[0]} & ${bridePName.split(' ')[0]}
+                Tia & Zaid
             </figcaption>`;
     };
 
