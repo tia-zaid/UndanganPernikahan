24593ee8fce47dbe126415f0997ef7d2cad7c352
tia-zaid <rhieskhiez.creative@gmail.com>
2025-09-25 00:11:54 +0700

    Update welcome.js

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -15,7 +15,7 @@ export const welcome = () => {
         return `
             <img src="${coupleImage}" alt="couple animation">
             <figcaption>
-                ${brideLName.split(' ')[0]} & ${bridePName.split(' ')[0]}
+                Tia & Zaid
             </figcaption>`;
     };
 
